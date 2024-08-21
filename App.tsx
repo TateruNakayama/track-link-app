b7807ac0bed6e60aef4d5487640d9d73b5c265b1
Tateru <68538759+TateruNakayama@users.noreply.github.com>
2024-08-21 21:49:50 +0900

    Revert "[UPDATE] サインインボタンのアラート表示を追加"

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import { useState, useEffect } from 'react'
 import { supabase } from './lib/supabase'
 import Auth from './components/Auth'
 import Account from './components/Account'
+import { View } from 'react-native'
 import { Session } from '@supabase/supabase-js'
-import { View, StyleSheet, Pressable, Text, } from 'react-native'
-
-// import=何を使うかを持ってくることを伝える
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null)
@@ -20,38 +18,9 @@ export default function App() {
     })
   }, [])
 
-  const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    buttonStyle: {
-        backgroundColor: "#2525ff",
-        width: 250,
-        height: 40,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 50
-    },
-    buttonText: {
-        color: "white",
-        fontWeight: "bold"
-    }
-});
-    // const=宣言CSS
-
-
   return (
-    <View style={styles.container}>
+    <View>
       {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-      <Pressable
-                style={styles.buttonStyle}
-                onPress={ () => alert("ボタンが押されました") }
-            >
-                <Text style={styles.buttonText}>Sign in</Text>
-            </Pressable>
     </View>
-    // View=出力
   )
 }
