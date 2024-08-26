4c4d2cc6e73b7e549e4130933627c4e91ac760e1
TateruNakayama <Gr4pu3@gmail.com>
2024-08-26 16:39:44 +0000

    [ADD] 必要なパッケージインストール

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
-  api.cache(true);
+const { plugin } = require("typescript-eslint")
+
+module.exports = function (api) {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    presets: ['babel-preset-expo']
+  }
+}
